d6190884c14dc79640855048e04e7bb9b5e2e4ae
x19990416 <starseeker.limin@gmail.com>
2016-11-22 09:47:46 +0800

    update

--- src/main/java/com/macrossx/springframework/Constants.java
+++ src/main/java/com/macrossx/springframework/Constants.java
@@ -6,7 +6,28 @@ public class Constants {
 	public static final Long REDIS_TIME_OUT = 60l;
 	public static final String message_success_code="000000";
 	
+
+	public static enum RESPONSE_MESSAGE {
+		M400("400","错误请求"),
+		M401("401","权限不够或未授权"),
+		M500("500","内部错误");
+		private String code;
+		private String msg;
+		RESPONSE_MESSAGE(String code, String msg) {
+			this.code = code;
+			this.msg = msg;
+		}
+
+		public String code() {
+			return code;
+		}
+
+		public String message() {
+			return msg;
+		}
+	}
 }
+
 
 
 
