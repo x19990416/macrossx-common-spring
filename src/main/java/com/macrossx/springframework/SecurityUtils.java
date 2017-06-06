a047ea4ebe7f87ca69e4b22ca1cc7283dc440ac6
x19990416 <starseeker.limin@gmail.com>
2017-06-06 11:04:25 +0800

    update for spring 4.3.8 Delete SecurtyUtil and merge md5 encoder to EncryptUtil

--- src/main/java/com/macrossx/springframework/SecurityUtils.java
+++
@@ -1,9 +0,0 @@
-package com.macrossx.springframework;
-
-import com.google.common.hash.Hashing;
-
-public class SecurityUtils {
-	public static String MD5(String message){
-		return  Hashing.md5().newHasher().putBytes(message.toString().getBytes()).hash().toString();
-	}
-}
